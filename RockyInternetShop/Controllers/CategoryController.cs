e827e5090e308c4336a5838049567b9723d6cb4e
IvanKrutik <ivan.krutik@gmail.com>
2023-10-27 19:32:10 +0400

    Rename

---
+++ RockyInternetShop/Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SampleInternetShop.Data;
+using SampleInternetShop.Models;
+
+namespace SampleInternetShop.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly AppDbContext _dbContext;
+
+        public CategoryController(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> categories = _dbContext.Category;
+            return View(categories);
+        }
+    }
+}
