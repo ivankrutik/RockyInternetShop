970132764aaa7102ab073fa5945995e9c3b3d14b
IvanKrutik <ivan.krutik@gmail.com>
2023-11-13 17:35:00 +0400

    Summary cshtml

---
+++ RockyInternetShop/Controllers/CartController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using RockyInternetShop.Data;
+using RockyInternetShop.Models;
+using RockyInternetShop.Models.ViewModel;
+using RockyInternetShop.Utility;
+using System.Security.Claims;
+
+namespace RockyInternetShop.Controllers
+{
+    [Authorize]
+    public class CartController : Controller
+    {
+        private readonly AppDbContext _appDbContext;
+
+        [BindProperty]
+        public ProductUserVM ProdUserVm { get; set; }
+
+        public CartController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public IActionResult Index()
+        {
+            var shpCarts = new List<ShoppingCart>();
+
+            if (HttpContext.Session.Get<List<ShoppingCart>>(WebConstant.SessionCart) != null &&
+                HttpContext.Session.Get<List<ShoppingCart>>(WebConstant.SessionCart).Any())
+            {
+                shpCarts = HttpContext.Session.Get<List<ShoppingCart>>(WebConstant.SessionCart);
+            }
+            var prodId = shpCarts.Select(z => z.ProductId);
+            var products = _appDbContext.Product.Where(z => prodId.Contains(z.Id));
+
+            return View(products);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Index")]
+        public IActionResult IndexPost()
+        {
+            return RedirectToAction(nameof(Summary));
+        }
+
+        public IActionResult Summary()
+        {
+            var UserId = User.FindFirstValue(ClaimTypes.Name);
+
+            var shpCarts = new List<ShoppingCart>();
+
+            if (HttpContext.Session.Get<List<ShoppingCart>>(WebConstant.SessionCart) != null &&
+                HttpContext.Session.Get<List<ShoppingCart>>(WebConstant.SessionCart).Any())
+            {
+                shpCarts = HttpContext.Session.Get<List<ShoppingCart>>(WebConstant.SessionCart);
+            }
+            var prodId = shpCarts.Select(z => z.ProductId);
+            var products = _appDbContext.Product.Where(z => prodId.Contains(z.Id));
+
+            ProdUserVm = new ProductUserVM
+            {
+                AppUser = UserId != null ? _appDbContext.AppUsers.FirstOrDefault(z => z.UserName == UserId) : new AppUser()
+            };
+
+            return View(ProdUserVm);
+        }
+
+        public IActionResult Remove(long id)
+        {
+            var shpCarts = new List<ShoppingCart>();
+
+            if (HttpContext.Session.Get<List<ShoppingCart>>(WebConstant.SessionCart) != null &&
+                HttpContext.Session.Get<List<ShoppingCart>>(WebConstant.SessionCart).Any())
+            {
+                shpCarts = HttpContext.Session.Get<List<ShoppingCart>>(WebConstant.SessionCart);
+                var obj = shpCarts.FirstOrDefault(z => z.ProductId == id);
+                if (obj != null)
+                {
+                    shpCarts.Remove(obj);
+                    HttpContext.Session.Set<List<ShoppingCart>>(WebConstant.SessionCart, shpCarts);
+                }
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+    }
+}
---
+++ RockyInternetShop/Models/ViewModel/ProductUserVM.cs
@@ -0,0 +1,14 @@
+﻿namespace RockyInternetShop.Models.ViewModel
+{
+    public class ProductUserVM
+    {
+        public ProductUserVM()
+        {
+            Products = new List<Product>();
+            AppUser = new AppUser();
+        }
+
+        public AppUser AppUser { get; set; }
+        public IEnumerable<Product> Products { get; set; }
+    }
+}
